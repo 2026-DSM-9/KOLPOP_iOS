c0507ec648bb69d5d53fe0dfcfb9cc15c9237b55
Juhui <joohee090331@gmail.com>
2026-07-13 14:40:28 +0900

    setting :: 프로젝트 세팅

---
+++ KOLPOP_iOS/KOLPOP_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KOLPOP_iOS
+//
+//  Created by 신주희 on 7/13/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
